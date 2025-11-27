1704ac926a4c24442b06c908311a7176c81061ce
macwsn <maciek.wisniewski04@gmail.com>
2025-11-27 18:22:17 +0100

    description

---
+++ Examples.py
@@ -0,0 +1,34 @@
+# Lokalizacja punktu na płaszczyźnie przy użyciu metody trapezowej
+
+Projekt polega na wyznaczaniu lokalizacji punktu na mapie przy użyciu metody trapezowej. Program został napisany w języku Python z użyciuem JupyterNotebook.
+
+## Zawartość repozytorium
+
+- `code.ipynb` - Główny plik programu realizujący obliczenia metodą trapezową.
+- `Examples/py` - Przykładowe dane wejściowe do testów programu.
+- `README.md` - Strona podstawowa
+- `Presentation.pttx` - Prezentacja
+- `Documentation.docs` - Dokumentacja
+- `/gifs` - Gify wizualizujące działanie tworzenia mapy trapzowej
+- `/requirements` - Wymagania, środowisko i preferowany opis uruchamiania 
+- `/requirements/visualizer.py` - Visualizer
+
+## Wymagania
+
+Wymaganie dostępne w pliku `/requirements/requriments.txt`
+
+## Uruchomienie programu
+
+Należy się zapoznać z zawartością pliku `/requirements/enviroment.txt`
+
+## Przykład działania
+
+<h5> GIF - wizualizacja tworzenia mapy trapezowej </h5>
+<img src="https://github.com/mcjwsn/Projekt-Geometryczne/blob/main/gifs/pr2.gif" width="700" height="700">
+<h5> Wynik algorytmu </h5>
+<img src="https://github.com/mcjwsn/Projekt-Geometryczne/blob/main/gifs/wiz4.png" width="700" height="700">
+
+## Autorzy 
+Oskar Blajsz [Blajszu](https://github.com/Blajszu)
+
+Maciej Wiśniewski [mcjwsn](https://github.com/mcjwsn)
